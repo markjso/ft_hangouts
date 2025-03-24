9e1422a090d6df5f4a30c6df58750c2a944e9f94
Jo Marks <97271827+markjso@users.noreply.github.com>
2025-03-24 17:17:17 +1030

    Update ViewContact.js

--- components/ViewContact.js
+++ components/ViewContact.js
@@ -1,17 +1,35 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { View, Text, StyleSheet, Pressable , Modal} from 'react-native';
 import ContactForm from './AddContact';
 import en from '../language/en.json';
 import fr from '../language/fr.json';
 import LanguageContext from '../context/LanguageContext';
+import { useContacts } from '../context/ContactContext';
 
-
-const ViewContact = ({route, navigation}) => {
-  const { firstName, name, nickname, phone, email, title } = route.params || {};
+const ViewContact = ({ route, navigation }) => {
+  const { id } = route.params || {};
+  const { fetchContacts, contacts } = useContacts();
+  const [contact, setContact] = useState({});
   const [showEditModal, setShowEditModal] = useState(false);
   const { language } = useContext(LanguageContext);
-  const locale = language === "fr" ? en : fr;
+  const locale = language === "en" ? en : fr;
 
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  useEffect(() => {
+    const updatedContact = contacts.find(c => c.id === id);
+    if (updatedContact) {
+      setContact(updatedContact);
+    }
+  }, [contacts, id]);
+
+  const handleModalClose = () => {
+    setShowEditModal(false);
+    fetchContacts();
+  };
+ 
   return ( 
     <View style={styles.container}>
       <Pressable style={styles.skillsLink} onPress={() => setShowEditModal(true)}>
@@ -19,28 +37,31 @@ const ViewContact = ({route, navigation}) => {
       </Pressable>
       <View style={styles.phoneBox}>
         <Text style = {styles.h6}>{locale.View.nickname}</Text>
-        <Text style={styles.h5}>{nickname}</Text>
+        <Text style={styles.h5}>{contact.nickname}</Text>
       </View>
       <View style={styles.phoneBox}>
         <Text style = {styles.h6}>{locale.View.phone}</Text>
-        <Text style={styles.h5}>{phone}</Text>
+        <Text style={styles.h5}>{contact.phone}</Text>
       </View>
       <View style={styles.phoneBox}>
         <Text style = {styles.h6}>{locale.View.email}</Text>
-        <Text style={styles.h5}>{email}</Text>
+        <Text style={styles.h5}>{contact.email}</Text>
       </View>
-     <Pressable style={styles.phoneBox} onPress={() => navigation.navigate('Messages', { title: `${firstName} ${name}`})}>
+     <Pressable style={styles.phoneBox} onPress={() => navigation.navigate('MessageUser', { receiverId: id, title: `${contact.firstName} ${contact.name}`})}>
         <Text style = {styles.h6}>{locale.View.send}</Text>
       </Pressable>
       <Modal 
         visible={showEditModal} 
         animationType="slide" 
-        transparent={false}>
+        transparent={false}
+        onRequestClose={() => setShowEditModal(false)}
+        >
         <View style={styles.modalOverlay}>
             <View style={styles.modalContainer}>
             <ContactForm
-              initialContact={{ firstName, name, nickname, phone, email, title }}
-              onSubmit={() => setShowEditModal(false)}
+              id={id}
+              existingContact={contact}
+              onSubmit={handleModalClose}
               onClose={() => setShowEditModal(false)}
             />
             </View>
